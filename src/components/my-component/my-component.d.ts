48433ea1b755d57d6abb82ccd8dc3211574e7f4c
ian xhio <xhio.ai@MacBook-Pro-de-Ian.local>
2023-12-30 01:42:41 -0600

    work completed

---
+++ src/components/my-component/my-component.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="react" />
+export declare class MyComponent {
+    first: string;
+    middle: string;
+    last: string;
+    private getText;
+    render(): import("react").JSX.Element;
+}
